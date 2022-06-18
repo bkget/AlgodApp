96e7907cd532333d65809c9d0f64109e921b6051
Biruk Getaneh <bkgetmom@gmail.com>
2022-06-18 04:59:37 +0300

    get transactions for the given address

--- application/indexer.py
+++ application/indexer.py
@@ -3,7 +3,7 @@ from algosdk.v2client import indexer
 
 
 def myindexer():
-    """Initialise and return an indexer"""
+    """Initialize and return an indexer"""
 
     algod_address = "https://testnet-algorand.api.purestake.io/idx2"
 
@@ -14,4 +14,33 @@ def myindexer():
     }
 
     return indexer.IndexerClient("", algod_address, headers)
+
+def get_transactions(address, substring):
+    """Returns a list of transactions related to the given address"""
+
+    response = myindexer().search_transactions(address=address, txn_type="pay")
+    txns = []
+    for txn in response["transactions"]:
+        sender = txn["sender"]
+        fee = txn["fee"]
+
+        amount = txn["payment-transaction"]["amount"]
+        if sender == address:
+
+            # if the current account is the sender, add fee and display transaction as negative
+            amount += fee
+            amount *= -1
+            other_address = txn["payment-transaction"]["receiver"]
+        else:
+            other_address = sender
+
+        amount /= microalgos_to_algos_ratio
+
+        # check for searched address
+        if substring not in other_address:
+            continue
+
+        txns.append({"amount": amount, "address": other_address})
+    return txns
+
 
