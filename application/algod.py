cacbdbc899c5d61a125f624169697ca5c1b5cfc0
Biruk Getaneh <bkgetmom@gmail.com>
2022-06-18 04:55:51 +0300

    wait for transaction confirmaition

--- application/algod.py
+++ application/algod.py
@@ -59,3 +59,35 @@ def send_txn(sender, quantity, receiver, note, sk):
         return False
 
 
+# utility for waiting on a transaction confirmation
+def wait_for_confirmation(transaction_id, timeout):
+    """
+    Wait until the transaction is confirmed or rejected, or until 'timeout'
+    number of rounds have passed.
+    Args:
+        transaction_id (str): the transaction to wait for
+        timeout (int): maximum number of rounds to wait
+    Returns:
+        dict: pending transaction information, or throws an error if the transaction
+            is not confirmed or rejected in the next timeout rounds
+    """
+
+    start_round = algod_client().status()["last-round"] + 1
+    current_round = start_round
+
+    while current_round < start_round + timeout:
+        try:
+            pending_txn = algod_client().pending_transaction_info(transaction_id)
+        except Exception as err:
+            print(err)
+            return
+        if pending_txn.get("confirmed-round", 0) > 0:
+            return pending_txn
+        elif pending_txn["pool-error"]:
+            raise Exception(
+                'pool error: {}'.format(pending_txn["pool-error"]))
+        algod_client().status_after_block(current_round)
+        current_round += 1
+    raise Exception(
+        'pending tx not found in timeout rounds, timeout value = : {}'.format(timeout))
+
