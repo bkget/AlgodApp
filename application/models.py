53b055d9021688ac905f2fbd0d872599e5913fbe
Biruk Getaneh <bkgetmom@gmail.com>
2022-06-18 05:11:26 +0300

    user account model: init function

---
+++ application/models.py
@@ -0,0 +1,14 @@
+from algosdk import mnemonic
+from flask_login import UserMixin
+
+from .algod import get_balance, send_txn, create_asset
+from .indexer import get_transactions, get_assets
+
+
+class User(UserMixin):
+    """User account model"""
+
+    def __init__(self, passphrase):
+        """Creates a user using the 25-word mnemonic"""
+        self.passphrase = passphrase
+
