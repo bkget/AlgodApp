f4cce2eb59546475f17a0f24d4403923a2bb05d6
Biruk Getaneh <bkgetmom@gmail.com>
2022-06-18 05:04:02 +0300

    send/recieve transaction route

--- application/views.py
+++ application/views.py
@@ -18,3 +18,16 @@ def index():
     return render_template('index.html', balance=balance)
 
 
+@main_bp.route('/send', methods=['GET', 'POST'])
+@login_required
+def send():
+    """Provides a form to create and send a transaction"""
+    form = SendForm()
+    address = current_user.public_key
+    if form.validate_on_submit():
+        success = current_user.send(form.quantity.data, form.receiver.data, form.note.data)
+        return render_template('success.html', success=success)
+
+    # show the form, it wasn't submitted
+    return render_template('send.html', form=form, address=address)
+
